903377ec83f2894886bbf83c27dac5b66cfe57bf
WKJBryan <bryanwangkangjie.03@gmail.com>
2026-08-30 22:56:18 +0800

    fix: embed Windows manifest in Rust tests

--- frontend/src-tauri/build.rs
+++ frontend/src-tauri/build.rs
@@ -1,4 +1,16 @@
 fn main() {
     #[cfg(feature = "tauri-app")]
-    tauri_build::build();
+    {
+        tauri_build::build();
+        if std::env::var_os("CARGO_CFG_TARGET_OS").as_deref()
+            == Some(std::ffi::OsStr::new("windows"))
+            && std::env::var_os("CARGO_CFG_TARGET_ENV").as_deref()
+                == Some(std::ffi::OsStr::new("msvc"))
+        {
+            println!("cargo:rustc-link-arg=/MANIFEST:EMBED");
+            println!(
+                "cargo:rustc-link-arg=/MANIFESTDEPENDENCY:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'"
+            );
+        }
+    }
 }
